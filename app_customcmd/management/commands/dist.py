50759b2d717091f1e281d4f3f2b430f25fd2e5bc
Elizabeth Hodges <elizabethhodges@MacBook-Pro.local>
2021-12-31 15:29:34 +0000

    automatically adding admin static files when disted

--- app_customcmd/management/commands/dist.py
+++ app_customcmd/management/commands/dist.py
@@ -1,12 +1,15 @@
 from django.core.management.base import BaseCommand
 import PyInstaller.__main__
 
+import subprocess
+
 
 class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
+        subprocess.run(["python", "manage.py", "collectstatic", "--noinput"])
         PyInstaller.__main__.run([
-            '-w',
             '--onefile',
-            'automatedPythonMarker.spec'
+            'automatedPythonMarker.spec',
+            '-w',
         ])
