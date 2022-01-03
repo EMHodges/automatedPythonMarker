510b3f3a69988fb3a36f8fc4dbb50d88ded0eeca
Elizabeth Hodges <elizabethhodges@MacBook-Pro.local>
2022-01-03 22:38:46 +0000

    getting static error messages in exe

--- static_lint/lint_answer.py
+++ static_lint/lint_answer.py
@@ -1,5 +1,50 @@
+import re
+from io import StringIO
+
 from static_lint.models import StaticLint
+from pylint import lint
+from pylint.reporters import text
+
+
+def write_answer_to_tmp_file(answer):
+    with open('code_to_lint.py', 'w') as tmp_file:
+        tmp_file.write(answer)
+    with open('code_to_lint.py', 'r') as tmp_file:
+        print(tmp_file.read())
 
 
 def lint_answer(answer, number):
-    StaticLint.objects.update_or_create(question_number=number, defaults={'feedback': answer})
+    write_answer_to_tmp_file(answer)
+
+    pylint_output = StringIO()  # need a StringIO object where the output will be stored
+    reporter = text.ColorizedTextReporter(pylint_output)
+    args = ["code_to_lint.py", "--enable=W,E,F"]
+    run = lint.Run(args, reporter=reporter, exit=False)  # exit=False means don't exit when the run is over
+    print(run)
+    print(pylint_output.getvalue())
+   # (pylint_stdout, pylint_stderr) = lint.py_run("code_to_lint.py", return_std=True)
+
+   # print(lint.py_run)
+   # print(pylint_stdout.getvalue())
+   # print('error')
+   # print(pylint_stderr.getvalue())
+   # print('end error')
+   # print(type(pylint_stdout.getvalue()))
+   # modified_outputs = pylint_stdout.getvalue().split("\n", 1)[1]
+   # unknown_line = re.compile(r"\(<unknown>, line \d*\)")
+   # modified_outputs = unknown_line.sub('', modified_outputs)
+   # print(modified_outputs, end="")
+
+    StaticLint.objects.update_or_create(question_number=number, defaults={'feedback': pylint_output.getvalue()})
+
+
+def format_lint_errors(lint_errors):
+    pass
+
+
+def format_lint_error(lint_error):
+    pass
+
+
+def format_lint_error_message(lint_message):
+    pass
