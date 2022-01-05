86d7a589a0efb4dc7c355f55ab1ef85872fc59f6
Elizabeth Hodges <elizabethhodges@MacBook-Pro.local>
2022-01-05 11:29:34 +0000

    formatting error messages to make them more readable

--- static_lint/lint_answer.py
+++ static_lint/lint_answer.py
@@ -1,44 +1,52 @@
+import json
 import os
 import re
 from io import StringIO
-from pathlib import Path
 
-from pylint.reporters.text import TextReporter
 from pylint.reporters.json_reporter import JSONReporter
-from automatedPythonMarker.settings import resource_path
 from static_lint.models import StaticLint
 from pylint import lint
-from pylint.reporters import text
 from astroid import MANAGER
 
 TMP_FILE = os.path.join('static_lint', 'code_to_lint.py')
 LINT_RULES_FILE = os.path.join('static_lint', '.pylintrc')
 
 
+def lint_answer(answer, number):
+    write_answer_to_tmp_file(answer)
+    lint_errors = get_lint_errors()
+    formatted_lint_errors = format_lint_errors(lint_errors)
+    return StaticLint.objects.update_or_create(question_number=number, defaults={'feedback': formatted_lint_errors})
+
+
 def write_answer_to_tmp_file(answer):
     with open(TMP_FILE, 'w') as tmp_file:
         tmp_file.write(answer)
-    with open(TMP_FILE, 'r') as tmp_file:
-        print(tmp_file.read())
 
 
-def lint_answer(answer, number):
-    write_answer_to_tmp_file(answer)
-
-    MANAGER.astroid_cache.clear()
-    pylint_output = StringIO()  # need a StringIO object where the output will be stored
+def get_lint_errors():
+    clear_pylint_cache()
+    pylint_output = StringIO()
     args = [TMP_FILE, '--errors-only']
-    lint.Run(args, reporter=JSONReporter(pylint_output), exit=False)  # exit=False means don't exit when the run is over
-    StaticLint.objects.update_or_create(question_number=number, defaults={'feedback': pylint_output.getvalue()})
+    lint.Run(args, reporter=JSONReporter(pylint_output), exit=False)
+    return pylint_output.getvalue()
+
+
+def clear_pylint_cache():
+    MANAGER.astroid_cache.clear()
 
 
 def format_lint_errors(lint_errors):
-    pass
+    lint_errors = json.loads(lint_errors)
+    formatted_lint_errors = [format_lint_error(lint_error) for lint_error in lint_errors]
+    return ''.join(formatted_lint_errors)
 
 
 def format_lint_error(lint_error):
-    pass
+    formatted_error_message = format_lint_error_message(lint_error['message'])
+    return f"line {lint_error['line']}, column {lint_error['column']}: {formatted_error_message}"
 
 
-def format_lint_error_message(lint_message):
-    pass
+def format_lint_error_message(message):
+    unknown_line = re.compile(r"\(<unknown>, line \d*\)")
+    return unknown_line.sub('', message)
