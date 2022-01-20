63921efc81c32e14821125626c76f7ed8bd63907
Elizabeth Hodges <elizabethhodges@macbook-pro.home>
2022-01-20 15:26:23 +0000

    adding total mark for question

--- questions/views.py
+++ questions/views.py
@@ -12,11 +12,15 @@ def question_update_view(request, number):
     obj = get_object_or_404(Question, id=number)
     form = QuestionForm(request.POST or None, instance=obj)
 
+    question = Question.objects.get(number=number)
+
     next_question = Question.objects.filter(number__gt=obj.number).order_by('number').first()
     previous_question = Question.objects.filter(number__lt=obj.number).order_by('number').last()
 
     static_errors = StaticLint.objects.get_or_none(question_number=number)
     test_results = Result.objects.filter(question_number=number)
+
+    print(Result.objects.total_mark_for_question(question_number=number))
 
     if form.is_valid():
         form.save()
@@ -27,6 +31,7 @@ def question_update_view(request, number):
         run_tests(form_answer, number)
         static_errors = StaticLint.objects.get(question_number=number)
         test_results = Result.objects.filter(question_number=number)
+        mark = Result.objects.total_mark_for_question(question_number=number)
 
     context = {
         'form': form,
@@ -34,7 +39,9 @@ def question_update_view(request, number):
         'next_question': next_question,
         'previous_question': previous_question,
         'static_errors': static_errors,
-        'test_results': test_results
+        'test_results': test_results,
+        'mark': Result.objects.total_mark_for_question(question_number=number),
+        'question': question
     }
     return render(request, "question/question.html", context)
 
