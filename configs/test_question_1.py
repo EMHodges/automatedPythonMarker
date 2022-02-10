647d74d55ea87b8f2868908851d753816167e6f6
Elizabeth Hodges <elizabethhodges@macbook-pro.home>
2022-02-10 12:06:50 +0000

    testing push

--- configs/test_question_1.py
+++ configs/test_question_1.py
@@ -9,24 +9,26 @@ from results.utils import setup_test, setup
 @RegisterTestClass(question_number=1)
 class TestQuestion1(QuestionsTestCase):
 
-    @setup(max_mark=6, test_params=[
-        TestCaseParameter(inputs={'a': 0, 'b': 0}, result=0, msg="hi"),
-        TestCaseParameter(inputs={'a': 0, 'b': 0}, result=0, msg="hi2"),
-    ])
-    def testNoSpeed(self, test_param: TestCaseParameter):
+ #   @setup(max_mark=6, test_params=[
+ #       TestCaseParameter(inputs={'a': 0, 'b': 0}, result=0, msg="hi"),
+ #       TestCaseParameter(inputs={'a': 0, 'b': 0}, result=0, msg="hi2"),
+ #   ])
+    @setup_test(max_mark=6)
+  #  def testNoSpeed(self, test_param: TestCaseParameter):
+    def testNoSpeed(self):
         from static_lint.code_to_lint import calculateFine
         """ Test that a value of 0 is returned when the speed is less than
         or equal to the speed limit.
         """
-        self.expectAlmostEqual(test_param.result, calculateFine(test_param.inputs['a'], test_param.inputs['b']), delta=0.000001, msg=test_param.message)
+        self.expectAlmostEqual(0, calculateFine(0,0), delta=0.000001, msg='0, 0')
 
     @setup_test(max_mark=6)
     def testSpeedLower90(self):
         from static_lint.code_to_lint import calculateFine
         """ Test that fines are calculated correctly for speed below 90 mph.
         """
-        self.expectAlmostEqual(150, calculateFine(60, 50), delta=0.000001, msg='60, 50')
         self.expectAlmostEqual(205, calculateFine(71, 50), delta=0.000001, msg='71, 50')
+        self.expectAlmostEqual(0, calculateFine(0, 0), delta=0.000001, msg='60, 50')
 
     @setup_test(max_mark=6)
     def testSpeedOver90(self):
