4871d5ae55f5bc097541f958f3de9771e521c946
Elizabeth Hodges <elizabethhodges@macbook-pro.home>
2022-02-28 15:08:06 +0000

    showing mark for each part of the question

--- results/templatetags/custom_filter.py
+++ results/templatetags/custom_filter.py
@@ -6,26 +6,21 @@ from results.models import Result
 
 @register.filter
 def get_item(dictionary, key):
-    print('get item')
-    print('dict')
-    print(dictionary)
-    print('key')
-    print(key)
     if not dictionary:
         return None
-    print('getting')
-    print(dictionary.get(key))
-    print('done getting')
     return dictionary.get(key)
 
 
 @register.filter
 def get_failing_subtest_params(result: Result):
-    print('failing subtest')
-    print(result)
     return result.get_failing_subtest_params()
 
 
 @register.filter
 def get_roman_numeral(number: int):
     return roman.toRoman(number).lower()
+
+
+@register.filter
+def get_mark(results):
+    return sum(results.values_list('mark', flat=True))
