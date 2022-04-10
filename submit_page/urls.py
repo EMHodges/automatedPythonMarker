607f9b8ec3743b556e0ea123addf5ef72ded476f
Elizabeth Hodges <elizabethhodges@MacBook-Pro.local>
2022-04-10 17:15:28 +0100

    removing unneeded submit page views

--- submit_page/urls.py
+++ submit_page/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 
-from .views import (submit_view, submit_page)
+from .views import (submit_view)
 
 app_name = 'submit_page'
 
 urlpatterns = [
-    path('', submit_page, name='submit-page'),
     path('submit_view/', submit_view, name='submit-view'),
 ]
--- submit_page/views.py
+++ submit_page/views.py
@@ -1,9 +1,6 @@
 import re
 import sys
-from io import StringIO
-
 from django.core import serializers
-from django.core.handlers.wsgi import WSGIRequest
 from django.http import HttpResponse
 
 # Create your views here.
@@ -13,49 +10,6 @@ from questions.models import QuestionComposite, SubQuestionComposite, TimeStarte
 from results.models import Result, Subtest
 from static_lint.models import StaticLint
 from submission.models import Submission
-
-
-def submit_page(request):
-    context = {}
-    return render(request, "submit_page/submit_page.html", context)
-
-def submit_views(request):
-    p = re.compile(r'(python-marker\d*).exe')
-    z = p.search(sys.argv[0])
-
-    print('yops')
-    print(z)
-
-    if z:
-        filename = z.group(1)
-    else:
-        filename = 'pythonMarker'
-
-    response = HttpResponse(content_type='text/plain')
-    response['Content-Disposition'] = f'attachment; filename={filename}.txt'
-
-    lines = []
-
-    questions = QuestionComposite.objects.all()
-
-    for question in questions:
-        subquestions = question.subquestioncomposite_set.all()
-
-        lines.append(serialize(subquestions))
-        lines.append(serialize(Result.objects.filter(question_number=question.number)))
-
-        for result in Result.objects.filter(question_number=question.number):
-                result_subtests = result.subtest_set.all()
-                print(result_subtests)
-                print(serialize(result_subtests))
-                lines.append(serialize(result_subtests))
-    print(lines)
-    response.writelines(lines)
-
-    return response
-
-
-
 
 
 def submit_view(request):
@@ -75,8 +29,6 @@ def submit_view(request):
 
     response = HttpResponse(content_type='text/plain')
     response['Content-Disposition'] = f'attachment; filename={filename}.txt'
-
-    run_all_tests()
 
     lines = []
     t = TimeStarted.objects.all()
@@ -100,24 +52,6 @@ def submit_view(request):
 
     return response
 
+
 def serialize(object):
     return serializers.serialize("yaml", object)
-
-def run_all_tests():
-    pass
-
-def fake_post_request(method=None, fake_user=False):
-    '''Returns a fake `WSGIRequest` object that can be passed to viewss.
-    If `fake_user` is `True`, we attach a random staff member to the request.
-    Even if not set, you can still do this manually by setting the `user`
-    attribute on the returned object.
-    The `GET` and `POST` `QueryDict` objects are mutable::
-        req = fake_request(mutable=True)
-        req.GET['q'] = 'abc'
-        my_view(req)
-    '''
-    request = WSGIRequest({
-        'REQUEST_METHOD': method or 'GET',
-        'wsgi.input': StringIO(),
-    })
-    return request
